1a939583ed9857c28574830fd06e83ab05111e09
sharyarnaveed <sharyarmalik430@gmail.com>
2025-04-17 16:03:57 +0500

    send otp

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,8 +1,7 @@
 const User = require("../models/user.models.js");
-const jwt =require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const { generateNUmber } = require("../utility/generateopt.js");
 const { sendopt } = require("../services/sendOpt.js");
-
 
 const signup = async (req, res) => {
   try {
@@ -19,43 +18,39 @@ const signup = async (req, res) => {
     } = req.body;
 
     console.log(process.env.SECRET_KEY);
-    
-const hashedpassword=await jwt.sign(password, process.env.HASHEDPASSWORD)
 
-const TheOpt=generateNUmber();
-console.log("otp",TheOpt);
+    const hashedpassword = await jwt.sign(password, process.env.HASHEDPASSWORD);
 
-const save=await User.create({
-  firstname,
-  lastname,
-  email,
-  password:hashedpassword,
-  phonenumber,
-  address,
-  city,
-  state,
-  zipcode:zip,
-otp:TheOpt
-})
+    const TheOpt = generateNUmber();
+    console.log("otp", TheOpt);
 
-if (!save) {
-  return res.status(400).json({ message: "Failed to save user" });
-  
-}
+    const save = await User.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedpassword,
+      phonenumber,
+      address,
+      city,
+      state,
+      zipcode: zip,
+      otp: TheOpt,
+    });
 
+    if (!save) {
+      return res.status(400).json({ message: "Failed to save user" });
+    }
 
-const Sendmail=await sendopt(TheOpt, email);
-if(!Sendmail) {
-  return res.status(400).json({ message: "Failed to send OTP" });
-}
-console.log("otp sent successfully");
-res.json("successfully signed up")
-
-
+    const Sendmail = await sendopt(TheOpt, email);
+    if (!Sendmail) {
+      return res.status(400).json({ message: "Failed to send OTP" });
+    }
+   
+    res.json("successfully signed up");
   } catch (error) {
-    console.log("error in sign up",error);
+    console.log("error in sign up", error);
     console.log(error.errors[0].message);
-    
+
     res.json({
       success: false,
       message: error.errors[0].message,
